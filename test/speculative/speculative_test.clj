6acedbcbeec778aef87aa622fab26e1a4e6711d1
Erik Assum <erik@assum.net>
2018-10-20 20:18:15 +0200

    Add speculative to circle

--- test/speculative/speculative_test.clj
+++ test/speculative/speculative_test.clj
@@ -1,7 +1,4 @@
 (ns speculative.speculative-test
   (:require [clojure.test :refer :all]
-            [speculative.speculative :refer :all]))
+            [speculative.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
