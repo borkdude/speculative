f363fc4caae93f0c8a18c5f29a002547abb63363
Mike Fikes <mike@fikesfarm.com>
2018-10-20 13:53:18 -0400

    merge spec

--- test/speculative/speculative_test.clj
+++ test/speculative/speculative_test.clj
@@ -1,7 +1,15 @@
 (ns speculative.speculative-test
-  (:require [clojure.test :refer :all]
-            [speculative.speculative :refer :all]))
+  (:require [clojure.spec.test.alpha :as stest]
+            [clojure.test :as t]
+            [speculative.core :as speculative] :reload))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(t/deftest merge-test
+  (stest/instrument `clojure.core/merge)
+  (t/is (merge {}))
+  (t/is (merge {} nil))
+  (t/is (nil? (merge nil)))
+  (t/is
+    (thrown-with-msg? clojure.lang.ExceptionInfo
+      #"Call to #'clojure.core/merge did not conform to spec"
+      (merge 1)))
+  (stest/unstrument `clojure.core/merge))
