4dec9e082a2aec7c8c9d1cca0f6a84a3bb15357e
Erik Assum <erik@assum.net>
2018-10-20 23:03:18 +0200

    Make lein understand where uname/passwd are comming from

--- project.clj
+++ project.clj
@@ -1,5 +1,11 @@
 (defproject speculative "0.0.2-SNAPSHOT"
   :description "A collection of specs for clojure.core functions"
   :url "https://github.com/slipset/speculative"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/test.check "0.9.0"]])
+                 [org.clojure/test.check "0.9.0"]]
+  :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
+                                  :username :env/clojars_username
+                                  :password :env/clojars_password
+                                  :sign-releases false}]])
