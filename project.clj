f163c9aef5f9ed12e390b2d33e2e89215a722448
Erik Assum <erik@assum.net>
2018-10-20 21:50:04 +0200

    Prepare for next release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject speculative "0.0.1"
+(defproject speculative "0.0.2-SNAPSHOT"
   :description "A collection of specs for clojure.core functions"
   :url "https://github.com/slipset/speculative"
   :dependencies [[org.clojure/clojure "1.9.0"]
