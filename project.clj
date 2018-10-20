831046f2c4d99c96f013fa4b6bcf4fab17a5f2c7
Erik Assum <erik@assum.net>
2018-10-21 00:36:30 +0200

    Upgrade to latest 1.10 RC for lein

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/slipset/speculative"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :dependencies [[org.clojure/clojure "1.10.0-RC1"]
                  [org.clojure/test.check "0.9.0"]]
   :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
                                   :username :env/clojars_username
