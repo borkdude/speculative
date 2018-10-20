edb9b9243e0e2759a1b57e34dfe88fe73da268d5
Erik Assum <erik@assum.net>
2018-10-20 17:01:06 +0200

    Add minimal project.clj for publishing to clojars

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject speculative "0.0.1"
+  :description "A collection of specs for clojure.core functions"
+  :url "https://github.com/cljsrn/side-fx"
+  :dependencies [[org.clojure/clojure "1.9"]
+                 [org.clojure/test.check "0.9.0"]])
