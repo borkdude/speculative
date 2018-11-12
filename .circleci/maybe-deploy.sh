9860874198ec522568a9cfccc9b162a741736074
Michiel Borkent <michielborkent@gmail.com>
2018-11-12 18:08:48 +0100

    Improve deploy script

--- .circleci/maybe-deploy.sh
+++ .circleci/maybe-deploy.sh
@@ -1,18 +1,8 @@
 #!/bin/sh
 
-if [ ! -z "$CIRCLE_PULL_REQUEST" ]
-then
-    echo "Skipped clojars push because this is a PR"
-    exit 0;
-fi
-
-echo "BRANCH $CIRCLE_BRANCH"
-echo "USERNAME $CIRCLE_USERNAME"
-
-if [ "$CIRCLE_BRANCH" = "master" ] && [ "$CIRCLE_USERNAME" = "slipset" ]
+if [ -z "$CIRCLE_PULL_REQUEST" ] && [ "$CIRCLE_BRANCH" = "master" ]
 then
     lein deploy clojars
-else
-    echo "Skipped clojars push because not master branch";
-    exit 0;
 fi
+
+exit 0;
