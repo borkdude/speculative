21ad728e3dad8711c383575aa919d5282819d6f9
Michiel Borkent <michielborkent@gmail.com>
2018-11-12 17:49:46 +0100

    Do not push to clojars when in PR

--- .circleci/maybe-deploy.sh
+++ .circleci/maybe-deploy.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
+if [ ! -z "$CIRCLE_PULL_REQUEST" ]
+then
+    echo "Skipped clojars push because this is a PR"
+    exit 0;
+fi
+
 if [ "$CIRCLE_BRANCH" = "master" ] && [ "$CIRCLE_USERNAME" = "slipset" ]
 then
     lein deploy clojars
 else
-    echo "Skipped command `lein deploy clojars`";
+    echo "Skipped clojars push because not master branch";
     exit 0;
 fi
