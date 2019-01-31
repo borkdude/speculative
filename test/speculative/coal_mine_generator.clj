00118fe281d05543373548b727969f697cb8f48c
Michiel Borkent <michielborkent@gmail.com>
2019-01-31 13:49:06 +0100

    Support running a range of coal-mine problems

--- test/speculative/coal_mine_generator.clj
+++ test/speculative/coal_mine_generator.clj
@@ -1,42 +1,51 @@
 (ns speculative.coal-mine-generator
   (:require
    [clojure.java.io :as io]
-   [coal-mine.script :as cm]))
+   [coal-mine.script :as cm]
+   [clojure.string :as str]))
 
 (def problem-numbers (set cm/problem-numbers))
 
-(defn ns-form [problem]
+(defn ns-form [problems]
   (format "(ns speculative.coal-mine-runner
 (:require %s
 #?(:clj patch.clj-2443)
 [speculative.instrument :refer [instrument]]
 [clojure.test])
-)" problem))
+)" (str/join " " problems)))
 
-(defn run-tests-form [problem]
+(defn run-tests-form [problems]
   (format "(defn run-tests []
 (println \"Instrumenting with speculative specs\")
 (instrument)
 (println \"Running tests %s\")
-(clojure.test/run-tests '%s))"
-          problem problem))
+(clojure.test/run-tests %s))"
+          (str/join ", " problems)
+          (str/join " " (map #(str "'" %) problems))))
 
 (defn run-form []
   "(time (run-tests))
 #?(:clj (shutdown-agents))")
 
-(defn emit-program [out problem]
-  (spit out (str (ns-form problem) "\n"))
-  (spit out (str (run-tests-form problem) "\n")
+(defn emit-program [out problems]
+  (spit out (str (ns-form problems) "\n"))
+  (spit out (str (run-tests-form problems) "\n")
         :append true)
   (spit out (str (run-form) "\n")
         :append true))
 
-(defn -main [& [out problem]]
-  (let [problem (or (and problem (Integer/parseInt problem))
-                    (rand-nth (seq problem-numbers)))
-        problem (if (contains? problem-numbers problem)
-                  (str "coal-mine.problem-" problem)
-                  (throw (Exception. (str "problem " problem " does not exist"))))]
+(defn -main [& [out problem-from problem-to]]
+  (let [[from to] [(when problem-from
+                     (Integer/parseInt problem-from))
+                   (when problem-to
+                     (Integer/parseInt problem-to))]
+        [from to] (if from [from (or to from)]
+                      (let [random (rand-nth (seq problem-numbers))]
+                        [random random]))
+        problem-range (range from (inc to))
+        problems (keep problem-numbers (range from (inc to)))
+        problems (if (seq problems)
+                   (mapv #(str "coal-mine.problem-" %) problems)
+                   (throw (Exception. (str "no problems found in range " (vec problem-range)))))]
     (io/make-parents out)
-    (emit-program out problem)))
+    (emit-program out problems)))
